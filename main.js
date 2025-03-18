112effe4bf23374de0d60d8a8209260b43660421
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-03-18 14:59:24 +0100

    lesson-1-js-finished

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log('Hello World!');
+
+let userName;
+userName = 'Victor';
+console.log(userName);
+
+let userAge;
+userAge = 21 ;
+console.log(userAge);
+
+let userFavoriteColor;
+userFavoriteColor = 'Green';
+console.log(userFavoriteColor);
+
+console.log(3 + 4);
+console.log(7 - 4);
+console.log(8 * 2);
+console.log(90 / 10);
+
+let x;
+x = 10;
+
+let y;
+y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(y / x);
